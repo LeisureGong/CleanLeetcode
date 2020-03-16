b89102f6af3f88b32461cc4adc39d4a21b3c83ad
gonglei <gonglei@encdata.cn>
2020-03-16 16:36:30 +0800

    罗马字母两道题

--- src/main/java/coding/Strings.java
+++ src/main/java/coding/Strings.java
@@ -1,5 +1,8 @@
 package coding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Strings {
 
     public static String gcdOfStrings(String str1, String str2) {
@@ -28,11 +31,83 @@ public class Strings {
         }
         return -1;
     }
+    //字符串压缩
+    public static String compressString(String S){
+        if(S.length() <= 2) return S;
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        char temp = S.charAt(0);
+        for(int i = 1;i < S.length();i++){
+            if(S.charAt(i) == temp){
+                count++;
+            }else{
+                sb.append(temp).append(count);
+                temp = S.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(temp).append(count);
+        if(sb.length() < S.length()){
+            return sb.toString();
+        }else{
+            return S;
+        }
+    }
+
+    //13. 罗马数字转整数
+    public static int romanToInt(String s){
+        Map<String,Integer> map = new HashMap<>();
+        map.put("I",1);
+        map.put("IV",4);
+        map.put("V",5);
+        map.put("IX",9);
+        map.put("X",10);
+        map.put("XL",40);
+        map.put("L",50);
+        map.put("XC",90);
+        map.put("C",100);
+        map.put("CD",400);
+        map.put("D",500);
+        map.put("CM",900);
+        map.put("M",1000);
+        int ans = 0;
+        for(int i = 0;i < s.length();){
+            if(i+1 < s.length() && map.containsKey(s.substring(i,i+2))){
+                ans += map.get(s.substring(i,i+2));
+                i += 2;
+            }else{
+                ans += map.get(s.substring(i,i+1));
+                i += 1;
+            }
+        }
+        return ans;
+    }
+    //12整数转罗马数字
+    public static String intToRoman(int num){
+        StringBuilder sb = new StringBuilder();
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};  // 罗马数字
+        int[] arab = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};  // 阿拉伯数字
+        int index = 0;
+        while(num > 0){
+            //贪心策略，找最大的先处理
+            int count = num / arab[index];
+            while(count-- > 0){
+                sb.append(roman[index]);
+            }
+            num %= arab[index];
+            index++;
+        }
+        return sb.toString();
+    }
+
 
 
 
     public static void main(String... args){
-        System.out.println(gcdOfStrings("aaaa","aa"));
+//        System.out.println(gcdOfStrings("aaaa","aa"));
+//        System.out.println(compressString(""));
+//        System.out.println(romanToInt("LVIII"));
+        System.out.println(intToRoman(3489));
     }
 
 
