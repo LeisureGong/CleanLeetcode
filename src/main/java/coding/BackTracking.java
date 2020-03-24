e728d815b00e6a791a3ae45cdd0e38c38f006212
gonglei <gonglei@encdata.cn>
2020-03-24 13:37:24 +0800

    回溯

--- src/main/java/coding/BackTracking.java
+++ src/main/java/coding/BackTracking.java
@@ -1,18 +1,74 @@
 package coding;
 
-import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author gonglei
  * @date 2020/3/9 11:52
  */
 public class BackTracking {
+
+	//22括号生辰 回溯
+	public static List<String> generateParenthesisUsingBackTrack(int n){
+		List<String> result = new ArrayList<>();
+		generateBackTracks(result,"",0,0,n);
+		return result;
+	}
+	public static void generateBackTracks(List<String> result,String temp,int l,int r,int n){
+		if(temp.length() == 2*n){
+			result.add(temp);
+			return;
+		}
+		if(l < n)
+			generateBackTracks(result,temp+"(",l+1,r,n);
+		if(r < l)
+			generateBackTracks(result,temp+")",l,r+1,n);
+	}
+
+	//22括号生成 DFS
+	public static List<String> generateParenthesis(int n) {
+		List<String> result = new ArrayList<>();
+		generateBackTrack(result,"",0,0,n);
+		return result;
+	}
+
+	public static void generateBackTrack(List<String> result,String temp,int l,int r,int n){
+		//剪枝
+		if(l > n || r > n || r > l) return;
+		//满足条件，保存结果
+		if(l == n && r == n){
+			result.add(temp);
+		}
+		generateBackTrack(result,temp+"(",l+1,r,n);
+		generateBackTrack(result,temp+")",l,r+1,n);
+		return;
+	}
+
+	//39.组合总和
+	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+		if(candidates.length <= 0 ) return Collections.emptyList();
+		List<List<Integer>> result = new ArrayList<>();
+		combinationBackTrack(result,new ArrayList<>(),target,0,candidates);
+		return result;
+	}
+
+	public static void combinationBackTrack(List<List<Integer>> result,List<Integer> temp,int target,int first,int[] candidates){
+		if(target < 0) return;
+		if(target == 0){
+			result.add(new ArrayList<>(temp));
+		}else{
+			for(int i = first;i < candidates.length;i++){
+				temp.add(candidates[i]);
+				//因为每个数字都可以使用无数次，所以递归还可以从当前元素开始
+				combinationBackTrack(result,temp,target-candidates[i],i,candidates);
+				temp.remove(temp.size() - 1);
+			}
+		}
+	}
 
 	//46全排列
 	public List<List<Integer>> permute(int[] nums){
@@ -55,7 +111,10 @@ public class BackTracking {
 		}
 	}
 
-
-
+	public static void main(String... args){
+//		System.out.println(generateParenthesis(10));
+//		System.out.println(combinationSum(new int[]{2,3,6,7},7));
+		System.out.println(generateParenthesisUsingBackTrack(3));
+	}
 
 }
