7a587ac9f09ff8a9d35c1333c7c2c08286699df8
gonglei <gonglei@encdata.cn>
2020-05-16 14:29:24 +0800

    链表逆转

--- src/main/java/coding/LinkedLists.java
+++ src/main/java/coding/LinkedLists.java
@@ -1,6 +1,9 @@
 package coding;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class LinkedLists {
 
@@ -13,29 +16,90 @@ public class LinkedLists {
         }
     }
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode preHead = new ListNode(-1);
-        ListNode prev = preHead;
-        //递归终止条件
-        while(l1 != null && l2 != null){
-            if(l1.val <= l2.val){
-                prev.next = l1;
-                l1 = l1.next;
-            }else{
-                prev.next = l2;
-                l2 = l2.next;
+    static class Solution {
+        public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode dummy = new ListNode(-1);
+            ListNode prev = dummy,curr = head;
+            dummy.next = head;
+            int len = 0;
+            while(head != null) {
+                len++;
+                head = head.next;
             }
-            prev = prev.next;
+            head = dummy.next;
+            for(int i = 0;i < len/k;i++) {
+                for(int j = 0;j < k - 1 ;j++){
+                    ListNode nextTemp = curr.next;
+                    curr.next = nextTemp.next;
+                    nextTemp.next = prev.next;
+                    prev.next = nextTemp;
+                }
+                prev = curr;
+                curr = prev.next;
+            }
+            return dummy.next;
         }
-        prev.next = l1 == null ? l2 : l1;
-        return preHead;
     }
 
-    public static void main(String... args){
 
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
 
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
     }
 
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+            line = in.readLine();
+            int k = Integer.parseInt(line);
+
+            ListNode ret = new Solution().reverseKGroup(head, k);
+
+            String out = listNodeToString(ret);
+
+            System.out.print(out);
+        }
+    }
 }
 
 
