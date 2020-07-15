791c4445d69da8726af0fac8ad06f7f007e480c3
gonglei <gonglei@encdata.cn>
2020-07-15 09:17:29 +0800

    _0713 星期2

--- src/main/java/coding/date/_0713.java
+++ src/main/java/coding/date/_0713.java
@@ -13,8 +13,49 @@ public class _0713 {
 
 	public static void main(String[] args) {
 		_0713 solution = new _0713();
-		String pattern = "aaa", str = "aa aa aa aa";
-		System.out.println(solution.countSegments("    foo    bar"));
+		int[] A = new int[]{0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+		int K = 3;
+		System.out.println(solution.longestOnes(A,K));
+	}
+
+	public int longestOnes(int[] A, int K) {
+
+		int[] B = new int[A.length];
+		int first, count, idxB = 0, idxA = 0;
+		while (idxA < A.length) {
+			first = A[idxA];
+			count = 1;
+			while (++idxA < A.length && A[idxA] == first) count++;
+			B[idxB++] = first == 0 ? -count : count;
+		}
+
+		// 保存最终结果
+		int res = 0, tempRes = 0, tempK = K;
+		int left, right = 0;
+		// 更新下第一个负数index,必为0或1
+		left = B[1] > 0 ? 2 : 1;
+		while (right < idxB) {
+			if (B[right] >= 0) {
+				tempRes += B[right];
+			} else {
+				// 先收缩左边的窗口，注意left是res的第二个数
+				if (left < right) {
+					while ( tempK + B[right] < 0) {
+						tempK -= B[left];
+						// 注意是减两个数
+						tempRes += B[left] - B[left - 1];
+					}
+					tempK += B[right];
+					tempRes -= B[right];
+				} else {
+					tempRes = 0;
+					tempK = K;
+				}
+			}
+			res = Math.max(res, tempRes);
+			right++;
+		}
+		return res;
 	}
 
 	public int countSegments(String s) {
