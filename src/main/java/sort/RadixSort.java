15ffb6b8208b8827155f5973d140afd4cb5d0d97
gonglei <gonglei@encdata.cn>
2020-10-19 15:46:40 +0800

    基数排序

--- src/main/java/sort/RadixSort.java
+++ src/main/java/sort/RadixSort.java
@@ -1,6 +1,19 @@
 package sort;
 
-public class RadixSort {
+import java.util.Arrays;
+
+public class RadixSort implements MyArraySort{
+
+    @Override
+    public int[] sort(int[] nums) throws Exception {
+
+        int[] arr = Arrays.copyOf(nums, nums.length);
+        // 获取数组中最大的数字
+        int maxVal = getMax(arr);
+        // 获取数字长度
+        int maxDigit = getNumLength(maxVal);
+        return radixSort(arr, maxDigit);
+    }
 
     /**
      * 获取数组中的最大元素
@@ -17,43 +30,81 @@ public class RadixSort {
         return max;
     }
 
-    /**
-     * 对数组按照照exp进行排序
-     * @param a 数组
-     * @param exp 指数
-     * @return
-     * @date 2020/10/14
-     */
-    private int[] expSort(int[] a, int exp) {
-        // 存储被排序数组
-        int[] output = new int[a.length];
-        // 10个桶
-        int[] buckets = new int[10];
-
-        // 每个桶的频率
-        for (int i = 0; i < a.length; i++)
-            buckets[(a[i]/exp)%10]++;
-
-        // 累加buckets[i]
-        for (int i = 1; i < 10; i++) {
-            buckets[i] += buckets[i-1];
+    // 计算数字长度
+    private int getNumLength(long num) {
+        if (num == 0) {
+            return 1;
         }
-
-        for (int i = a.length - 1; i >= 0; i--) {
-            output[buckets[(a[i]/exp)%10] - 1] = a[i];
-            buckets[(a[i]/exp)%10]--;
+        int length = 0;
+        for (long tmp = num; tmp != 0; tmp /= 10) {
+            length++;
         }
-        return  output;
+        return length;
     }
+
+//    /**
+//     * 对数组按照照exp进行排序
+//     * @param a 数组
+//     * @param exp 指数
+//     * @return
+//     * @date 2020/10/14
+//     */
+//    private int[] expSort(int[] a, int exp) {
+//        // 存储被排序数组
+//        int[] output = new int[a.length];
+//        // 10个桶
+//        int[] buckets = new int[10];
+//
+//        // 每个桶的频率
+//        for (int i = 0; i < a.length; i++)
+//            buckets[(a[i]/exp)%10]++;
+//
+//        // 累加buckets[i]
+//        for (int i = 1; i < 10; i++) {
+//            buckets[i] += buckets[i-1];
+//        }
+//
+//        for (int i = a.length - 1; i >= 0; i--) {
+//            output[buckets[(a[i]/exp)%10] - 1] = a[i];
+//            buckets[(a[i]/exp)%10]--;
+//        }
+//        return  output;
+//    }
 
     /**
      * 对数组进行基数排序
-     * @param a
+     * @param arr
      * @return
      * @date 2020/10/14
      */
-    public void radixSort(int[] a) {
-        int max = getMax(a);
+    public int[] radixSort(int[] arr, int maxDigit) {
+        int mod = 10;
+        int dev = 1;
 
+        for (int i = 0; i < maxDigit; i++, dev *= 10, mod *= 10) {
+            // 考虑负数的情况，扩展一倍队列数， 其中[0-9]对应负数，[10-19]对应正数
+            int[][] counter = new int[mod * 2][0];
+
+            for (int j = 0; j < arr.length; j++) {
+                int bucket = ((arr[j] % mod) / dev) + mod;
+                counter[bucket] = arrAppend(counter[bucket], arr[j]);
+            }
+
+            int pos = 0;
+            for (int[] bucket : counter) {
+                for (int val : bucket) {
+                    arr[pos++] = val;
+                }
+            }
+        }
+        return arr;
     }
+
+    // 自动扩容，并保存数据
+    private int[] arrAppend(int[] arr, int val) {
+        arr = Arrays.copyOf(arr, arr.length + 1);
+        arr[arr.length - 1] = val;
+        return arr;
+    }
+
 }
